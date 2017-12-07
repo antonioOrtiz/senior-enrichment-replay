f0d1d8b43546b6abbdd2f96b975bc75cc9e1c3af
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-12-06 23:53:05 -0500

    changed db.sync-removal of force:true

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const db = require('./server/db/_db');
 const app = require('./server');
 const PORT = 1337;
-
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
-.then(() => {
-  console.log('db synced')
-  app.listen(PORT, () => console.log(`server flyin on port ${PORT}`))
-});
+// { force: true }
+db
+  .sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+  .then(() => {
+    console.log('db synced');
+    app.listen(PORT, () => console.log(`server flyin on port ${PORT}`));
+  });
