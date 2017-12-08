8e627f2e9b2c47f1ffd5c904d973c501bc876c30
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-12-08 09:11:33 -0500

    finally got syntax right for adding new state to arrays! using new ESsyntax

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,12 +1,44 @@
 /* combineReducers is not currently used, but eventually should be for modular code :D */
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import axios from 'axios';
 
-const initialState = {}
+const initialState = {
+  topFiveCountries: [],
+};
 
+// ACTION TYPES
+
+const GET_TOP_COUNTRIES_BY_GFI = 'GET_TOP_COUNTRIES_BY_GFI';
+
+// ACTION CREATORS
+export function getTopFiveCountriesByGFI(countries) {
+  const action = {
+    type: GET_TOP_COUNTRIES_BY_GFI,
+    countries,
+  };
+  return action;
+}
+
+export function fetchTopFiveCountriesByGFI() {
+  return function thunk(dispatch) {
+    return axios
+      .get('/api/countries/top-five-countries')
+      .then(res => res.data)
+      .then(countries => {
+        const action = getTopFiveCountriesByGFI(countries);
+        dispatch(action);
+      });
+  };
+}
+
+// REDUCER
 const rootReducer = function(state = initialState, action) {
-  switch(action.type) {
-    default: return state
+  switch (action.type) {
+    case GET_TOP_COUNTRIES_BY_GFI:
+      return { ...state, topFiveCountries: action.countries };
+    default:
+      return state;
   }
 };
 
-export default rootReducer
+export default rootReducer;
