2dcfcc0696d0948765802ae030c1f22f1cb4c077
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-12-09 22:03:21 -0500

    Fixed bug in client/components/TopFiveCountriesByGFI.js

---
+++ client/components/TopFiveCountriesByGFI.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+export default function TopFiveCountriesByGFI(props) {
+  const topFiveCountries = props.topFiveCountries;
+
+  const flagStyle = {
+    height: '50px',
+    width: '100px',
+  };
+  return (
+    <div className="row">
+      <div className="twelve columns">
+        <h2> - Top Five Coutries By GFI -</h2>
+        <table className="u-full-width">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>GFI</th>
+              <th>Flag </th>
+            </tr>
+          </thead>
+
+          {topFiveCountries.map(function(country) {
+            return (
+              <tbody key={country.id}>
+                <tr>
+                  <td>
+                    <Link className="country-page-link" to={`/countries/${country.id}`}>
+                      {country.name}
+                    </Link>
+                  </td>
+                  <td>{country.GFI}</td>
+                  <td>
+                    <img style={flagStyle} src={country.flagUrl} />
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
+    </div>
+  );
+}
