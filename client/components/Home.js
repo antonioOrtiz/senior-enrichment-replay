126a746e5ad63a4f30fb8ddd98bb77f914e662d9
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-12-09 21:59:29 -0500

    made this into dumb component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,35 +1,15 @@
 import React from 'react';
+import TopFiveCountriesByGFI from './TopFiveCountriesByGFI';
 
-const Home = () => (
-  <div className="row">
-    <div className="twelve columns">
-      <h1>- Welcome to the Senior Enrichment Project -</h1>
-      <table className="u-full-width">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Dave Gamache</td>
-            <td>26</td>
-            <td>Male</td>
-            <td>San Francisco</td>
-          </tr>
-          <tr>
-            <td>Dwayne Johnson</td>
-            <td>42</td>
-            <td>Male</td>
-            <td>Hayward</td>
-          </tr>
-        </tbody>
-      </table>
+export default function Home(props) {
+  const topFiveCountries = props.topFiveCountries;
+
+  return (
+    <div className="row">
+      <div className="twelve columns">
+        <h1>- Welcome to the Senior Enrichment Project -</h1>
+        <TopFiveCountriesByGFI topFiveCountries={topFiveCountries} />
+      </div>
     </div>
-  </div>
-);
-
-export default Home;
+  );
+}
