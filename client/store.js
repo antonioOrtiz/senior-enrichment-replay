8be7ac672a50fc3199b3c858e66e5508288c6df7
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-12-10 20:39:03 -0500

    worked in boilerplate to wire everything with react-redux

--- client/store.js
+++ client/store.js
@@ -1,6 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import loggingMiddleware from 'redux-logger'; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from 'redux-thunk'; // https://github.com/gaearon/redux-thunk
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default createStore(rootReducer, applyMiddleware(thunkMiddleware, loggingMiddleware))
+import topFiveCountriesByGFIReducer from './reducers/topFiveCountriesByGFI';
+import countriesReducer from './reducers/countries';
+import oneCountryReducer from './reducers/oneCountry';
+
+const middleware = [loggingMiddleware, thunkMiddleware];
+
+const rootReducer = combineReducers({
+  topFiveCountries: topFiveCountriesByGFIReducer,
+  countries: countriesReducer,
+  oneCountry: oneCountryReducer,
+});
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
